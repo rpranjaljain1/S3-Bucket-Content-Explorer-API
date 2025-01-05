d7faff75c1d6dc69c9afd89a1ebf8d906a3c767d
Pranjal Jain <126986072+rpranjaljain1@users.noreply.github.com>
2025-01-05 23:33:58 +0530

    Create main.tf

---
+++ Terraform/modules/EC2/Security-Group/main.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "this" {
+  name        = var.security_group_name
+  description = "Security group for EC2 with SSH (22) and Web (5000) access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
